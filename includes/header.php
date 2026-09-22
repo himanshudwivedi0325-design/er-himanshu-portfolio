39e1a1efbf43e8a123c91d0e18c8e5a3057553f3
Himanshu <Himanshudwivedi0325@gmail.com>
2026-09-21 22:54:04 -0700

    Add Google Search Console verification meta tag

--- includes/header.php
+++ includes/header.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <title>Er. Himanshu Dwivedi | Official Portfolio & Engineer Profile</title>
+    <meta name="google-site-verification" content="GoZCIITzfilwYdjE50zzVO_twpzOIHwPqdpPj3AsQwg" />
     
     <!-- Meta Tags -->
     <meta
